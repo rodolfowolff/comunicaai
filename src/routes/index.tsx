5eb4ecdda65349624326959a037f88a9373a13ca
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-06-24 19:41:49 -0300

    feat: check user aut

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 
 import { Home } from "../screens/Home";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  useEffect(() => {
+    const subscribe = auth().onAuthStateChanged(setUser);
+    return subscribe;
+  }, []);
+
   return (
     <NavigationContainer>
-      {/* <Home /> */}
-      <AuthRoutes />
+      {user ? <Home /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }

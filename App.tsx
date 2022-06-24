28b17fe86f8bd03d5ced86b43ab84ad33bc52904
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-06-23 21:35:47 -0300

    feat: loadin font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,48 @@
+import React, { useCallback, useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
+import { preventAutoHideAsync, hideAsync } from "expo-splash-screen";
+import Entypo from "@expo/vector-icons/Entypo";
+import { ThemeProvider } from "styled-components/native";
 import { Text, View } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+import { loadAsync } from "expo-font";
+import theme from "./src/theme";
 
 export default function App() {
+  // const [appIsReady, setAppIsReady] = useState(false);
+  const [fontsLoaded] = useFonts({
+    Inter_700Bold,
+    Inter_400Regular,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar style="dark" translucent backgroundColor="transparent" />
+      <View
+        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        onLayout={onLayoutRootView}
+      >
+        <Text style={{ fontFamily: theme.FONTS.SIZES.BOLD }}>
+          SplashScreen Demo! 👋
+        </Text>
+        <Text style={{ fontFamily: theme.FONTS.SIZES.NORMAL }}>
+          SplashScreen Demo! 👋
+        </Text>
+      </View>
+    </ThemeProvider>
   );
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,26 @@
+export default {
+  COLORS: {
+    BACKGROUND: "#F3F2F8",
+
+    PRIMARY: "#55CB97",
+    SECONDARY: "#03314B",
+
+    TEXT: "#383B43",
+    SUBTEXT: "#8D919E",
+
+    WHITE: "#FFFFFF",
+    BORDER: "#C5CADA",
+  },
+
+  FONTS: {
+    SIZES: {
+      BOLD: "Inter_700Bold",
+      NORMAL: "Inter_400Regular",
+    },
+    WEIGHTS: {
+      light: "300",
+      regular: "500",
+      bold: "700",
+    },
+  },
+};
